ac75a78023239037e99dd524ecbb9272bda86205
dneb8 <107234871+dneb8@users.noreply.github.com>
2025-03-17 22:40:30 -0600

    Función para agregar amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,14 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre) {
+        amigos.push(nombre);
+        console.log("Lista de amigos:", amigos); //BORRAR CONSOLE LOG
+        input.value = ""; 
+    } else {
+        alert("Por favor, ingresa un nombre válido.");
+    }
+}
